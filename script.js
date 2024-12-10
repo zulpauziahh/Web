c4d35585551401c08b3c73e6dede48c132090652
zulpauziahh <zlpapzhsenn@gmail.com>
2024-12-10 11:01:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -289,7 +289,7 @@ out+=`
 <div class="artikel ">
 
 <p>${svg.view.icon(val.icon)}</p>
-<b>Twitter</b>
+<b>Web Online</b>
 <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 <p><a class="" href="#" >View details</a></p>
 </div>
